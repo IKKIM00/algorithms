5b86c281a4d08a2378d5755bb8c449e1a6f40af2
IKKIM00 <kimjennifer0429@gmail.com>
2021-04-21 15:02:57 +0900

    create leetcode_Reverser_String.py

---
+++ leetcode_Reverser_String.py
@@ -0,0 +1,17 @@
+"""
+문제 설명:
+주어진 string s를 reverse하는 것
+그런데 새로운 변수에 할당하는 것이 아니라 주어진 변수만 활용해서 해야 함
+
+예시:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
